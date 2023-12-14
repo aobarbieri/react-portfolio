dac61bd1876b4af91adc200431133b14dfccd894
aobarbieri <aobarbieri@gmail.com>
2023-12-14 15:40:01 -0500

    Update About page to use json data

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,43 +1,39 @@
-import avatar from '../assets/avatar.png'
+import { useState, useEffect } from 'react'
 
 export default function About() {
-	return (
+	const [about, setAbout] = useState(null)
+
+	const getAboutData = async () => {
+		const res = await fetch('./about.json')
+		const data = await res.json()
+		setAbout(data)
+	}
+	useEffect(() => {
+		getAboutData()
+	}, [])
+
+	const loaded = () => (
 		<section className='home'>
 			{/* Avatar */}
-			<section className='headline load-hidden flex mb-16'>
-				<img className='avatar border rounded-full mr-6' src={avatar} alt="Amanda's Avatar" />
+			<section className='flex mb-16'>
+				<img className='mr-6' src={about.headshot} alt="Amanda's Avatar" />
 				<div className='self-center'>
-					<h1 className='font-semibold text-4xl'>Amanda</h1>
+					<h1 className='font-semibold text-4xl'>{about.name}</h1>
 					<h2 className='text-sm leading-6 font-light'>Full Stack Developer</h2>
+					<h2 className='text-sm leading-6 font-light'>
+						{about.email}
+					</h2>
 				</div>
 			</section>
 
 			{/* About */}
-			<article className='headline load-hidden mb-4'>
+			<section className='mb-4'>
 				<div className='md:text-lg'>
-					<p>
-						I'm a self taught developer learning software engineering by doing. I enjoy working with challenging layouts and building web pages that
-						are user-friendly, simple and delightful.
-					</p>
+					<p>{about.bio}</p>
 				</div>
-			</article>
-
-			{/* Buttons */}
-			<article className='about punchline load-hidden flex flex-nowrap my-12 lg:mb-0 text-sm md:text-lg'>
-				<a className='' href='resume'>
-					<i className='fa-solid fa-graduation-cap mr-1.5 link-icon rounded-xl transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'></i>{' '}
-					Resume
-				</a>
-				<a className='workLink mx-5' href='#work'>
-					<i className='fa-solid fa-rocket mr-1.5 link-icon rounded-xl transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'></i>{' '}
-					Work
-				</a>
-
-				<a className='contactLink' href='#contact'>
-					<i className='fa-solid fa-comment-dots mr-1.5 link-icon rounded-xl transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'></i>{' '}
-					Contact
-				</a>
-			</article>
+			</section>
 		</section>
 	)
+
+	return about ? loaded() : <h1>Loading...</h1>
 }
