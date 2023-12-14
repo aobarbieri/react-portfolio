4f8b7040b25d70e7628ea467098153de669d559d
aobarbieri <aobarbieri@gmail.com>
2023-12-14 15:18:26 -0500

    Add routes for projects, about and resume pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import Projects from './pages/Projects'
-import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
 import About from './pages/About'
+import Projects from './pages/Projects'
 
 function App() {
 	return (
 		<>
-      <Header />
-      <About />
-			<Projects />
+			<Header />
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='/projects' element={<Projects />} />
+				<Route path='/about' element={<About />} />
+			</Routes>
 			<Footer />
 		</>
 	)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,29 @@
+import { Link } from 'react-router-dom'
 import logo from '../assets/html-logo.svg'
 
 export default function Header() {
 	return (
 		<header className='fixed w-full'>
 			<nav className='navbar'>
-				<a className='flex items-end' href='#start'>
+				<Link to='/' className='flex items-end'>
 					<img className='hidden md:inline md:h-7' src={logo} alt='HTML icon' />
 					<h3 className='text-lg md:text-xl font-semibold mx-2'>Amanda Mitzian</h3>
-				</a>
+				</Link>
 
 				{/* Tablet and Laptop */}
 				<ul className='navbar__full'>
-					<li>
-						<a href='resume'>Resume</a>
-					</li>
-					<li className='mx-5'>
-						<a className='workLink'>
-							Work
-						</a>
-					</li>
-					<li>
-						<a href='#contact'>Contact</a>
-					</li>
+					<Link to='/'>
+						<li className='mx-5'>Home</li>
+					</Link>
+					<Link to='/about'>
+						<li>About</li>
+					</Link>
+					<Link to='/projects'>
+						<li className='mx-5'>Projects</li>
+					</Link>
+					<Link to='/resume'>
+						<li>Resume</li>
+					</Link>
 				</ul>
 			</nav>
 		</header>
