34960617e6e6dd480e542e10d7b56d698e0397bd
aobarbieri <aobarbieri@gmail.com>
2024-01-25 17:12:41 -0500

    Install SASS, uninstall Tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,20 @@
 import { Link } from 'react-router-dom'
-import logo from '../assets/html-logo.svg'
 
 export default function Header() {
 	return (
 		<header className='fixed'>
 			<nav className='navbar flex justify-between'>
 				<Link to='/' className='flex items-end'>
-					<img className='hidden md:inline md:h-7' src={logo} alt='HTML icon' />
-					<h3 className='text-lg md:text-xl font-semibold mx-2'>Amanda Mitzian</h3>
+					<h3 className=''>Amanda Mitzian</h3>
 				</Link>
 
 				{/* Tablet and Laptop */}
 				<ul className='flex items-end'>
-					<Link to='/'>
-						<li className='mx-5'>Home</li>
-					</Link>
 					<Link to='/projects'>
 						<li>Projects</li>
 					</Link>
+					<li>Resume</li>
+					<li>Contact</li>
 				</ul>
 			</nav>
 		</header>
