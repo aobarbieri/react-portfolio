0c6c53378459e921d4ffb1a2518f55af82a6e251
aobarbieri <aobarbieri@gmail.com>
2024-01-30 18:11:39 -0500

    Add projects, add more tech icons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,14 +4,14 @@ import github from '../assets/brands/github.svg'
 import html from '../assets/brands/html.svg'
 import css from '../assets/brands/css.svg'
 import sass from '../assets/brands/sass.svg'
-// import tailwind from '../assets/brands/tailwind.svg'
+import tailwind from '../assets/brands/tailwind.svg'
 import js from '../assets/brands/js.svg'
 import express from '../assets/brands/express.svg'
-// import nextJs from '../assets/brands/next-js.svg'
-// import react from '../assets/brands/react.svg'
+import nextJs from '../assets/brands/next-js.svg'
+import react from '../assets/brands/react.svg'
 import mongodb from '../assets/brands/mongodb.svg'
 import mongoose from '../assets/brands/mongoose.svg'
-// import postgre from '../assets/brands/postgre.svg'
+import postgre from '../assets/brands/postgre.svg'
 
 export default function Projects() {
 	const [projects, setProjects] = useState(null)
@@ -48,13 +48,21 @@ export default function Projects() {
 
 								{project.tech.sass ? <img className='tech-icon' src={sass} alt='SASS' title='SASS' /> : ''}
 
+								{project.tech.tailwind ? <img className='tech-icon' src={tailwind} alt='Tailwind' title='Tailwind' /> : ''}
+
 								{project.tech.js ? <img className='tech-icon' src={js} alt='JavaScript' title='JavaScript' /> : ''}
 
 								{project.tech.express ? <img className='tech-icon' src={express} alt='Express.js' title='Express.js' /> : ''}
 
+								{project.tech.nextJs ? <img className='tech-icon' src={nextJs} alt='Next.js' title='Next.js' /> : ''}
+
+								{project.tech.react ? <img className='tech-icon' src={react} alt='React' title='React' /> : ''}
+
 								{project.tech.mongodb ? <img className='tech-icon' src={mongodb} alt='Mongo DB' title='Mongo DB' /> : ''}
 
 								{project.tech.mongoose ? <img className='tech-icon' src={mongoose} alt='Mongoose' title='Mongoose' /> : ''}
+
+								{project.tech.postgre ? <img className='tech-icon' src={postgre} alt='PostgreSQL' title='PostgreSQL' /> : ''}
 							</div>
 
 							<a className='btn-cta' href={project.live} target='_blank' rel='noreferrer'>
