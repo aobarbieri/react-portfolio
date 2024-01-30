e86ff8218690f0a5146a367452be580782cb786c
aobarbieri <aobarbieri@gmail.com>
2024-01-30 12:35:43 -0500

    Styles update for About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import avatar from '../assets/avatar.svg'
+import bg from '../assets/avatar-bg.png'
 
 export default function About() {
 	const [about, setAbout] = useState(null)
@@ -22,11 +23,7 @@ export default function About() {
 
 			<div className='wrapper'>
 				<img className='avatar' src={avatar} alt="Amanda's Avatar" />
-
-				<svg className='avatar-bg' width='100%' height='206' viewBox='0 0 100% 206' fill='none' xmlns='http://www.w3.org/2000/svg'>
-					<path d='M0 0C552.627 26.641 867.838 27.1125 1440 0V174.873C879.307 202.275 563.903 201.137 0 174.873V0Z' fill='#99D3BE' />
-					<path d='M0 157C563.257 181.964 876.893 180.619 1440 157V185C876.419 212.031 560.397 211.374 0 185V157Z' fill='#629A86' />
-				</svg>
+				<img className='avatar-bg' src={bg} />
 			</div>
 		</section>
 	)
