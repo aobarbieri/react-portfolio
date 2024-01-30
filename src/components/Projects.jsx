7087943a5fb763e3812918263dd7fce6a0872e7d
aobarbieri <aobarbieri@gmail.com>
2024-01-30 14:03:18 -0500

    dynamic icons for projects-tech

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,18 @@
 import { useState, useEffect } from 'react'
 import github from '../assets/brands/github.svg'
 
+import html from '../assets/brands/html.svg'
+import css from '../assets/brands/css.svg'
+import sass from '../assets/brands/sass.svg'
+// import tailwind from '../assets/brands/tailwind.svg'
+import js from '../assets/brands/js.svg'
+import express from '../assets/brands/express.svg'
+// import nextJs from '../assets/brands/next-js.svg'
+// import react from '../assets/brands/react.svg'
+import mongodb from '../assets/brands/mongodb.svg'
+import mongoose from '../assets/brands/mongoose.svg'
+// import postgre from '../assets/brands/postgre.svg'
+
 export default function Projects() {
 	const [projects, setProjects] = useState(null)
 
@@ -25,7 +37,23 @@ export default function Projects() {
 						<h3>{project.name}</h3>
 						<img src={project.image} alt={project.name} />
 						<p className='description'>{project.description}</p>
-						<p className='tech'>Built with:</p>
+						<div className='tech'>
+							<p>Built with:</p>
+
+							{project.tech.html ? <img className='tech-icon' src={html} alt='HTML' title='HTML' /> : ''}
+
+							{project.tech.css ? <img className='tech-icon' src={css} alt='CSS' title='CSS' /> : ''}
+
+							{project.tech.sass ? <img className='tech-icon' src={sass} alt='SASS' title='SASS' /> : ''}
+
+							{project.tech.js ? <img className='tech-icon' src={js} alt='JavaScript' title='JavaScript' /> : ''}
+
+							{project.tech.express ? <img className='tech-icon' src={express} alt='Express.js' title='Express.js' /> : ''}
+
+							{project.tech.mongodb ? <img className='tech-icon' src={mongodb} alt='Mongo DB' title='Mongo DB' /> : ''}
+
+							{project.tech.mongoose ? <img className='tech-icon' src={mongoose} alt='Mongoose' title='Mongoose' /> : ''}
+						</div>
 
 						<a className='btn-cta' href={project.live} target='_blank' rel='noreferrer'>
 							Try it out
