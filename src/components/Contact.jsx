f9db9db57afcd5f3a83c2cbf62f9f6f9bbf73657
aobarbieri <aobarbieri@gmail.com>
2024-02-01 13:40:28 -0500

    href for aside list items

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import github from '../assets/brands/github.svg'
 import linkedin from '../assets/brands/linkedin.svg'
-import email from '../assets/icons/email.svg'
+// import email from '../assets/icons/email.svg'
 import resume from '../assets/icons/resume.svg'
 
 export default function Contact() {
@@ -8,25 +8,25 @@ export default function Contact() {
 		<aside className='floating-list'>
 			<ul>
 				<li className='list-item'>
-					<a href=''>
+					<a href='https://www.linkedin.com/in/amanda-mitzian/' target='_blank' rel='noreferrer'>
 						LinkedIn
 						<img src={linkedin} alt='' />
 					</a>
 				</li>
 				<li className='list-item'>
-					<a href=''>
+					<a href='https://github.com/aobarbieri' target='_blank' rel='noreferrer'>
 						GitHub
 						<img src={github} alt='' />
 					</a>
 				</li>
-				<li className='list-item'>
-					<a href=''>
+				{/* <li className='list-item'>
+					<a href='' target='_blank' rel='noreferrer'>
 						Email
 						<img src={email} alt='' />
 					</a>
-				</li>
+				</li> */}
 				<li className='list-item'>
-					<a href=''>
+					<a href='https://drive.google.com/file/d/1zhjTywdUQ9HmZyuEaP6MT1hUastRY-n9/view?usp=sharing' target='_blank' rel='noreferrer'>
 						Resume
 						<img src={resume} alt='' />
 					</a>
