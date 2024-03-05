db19cd60c7facb25a3ff9845114636948a30ad57
aobarbieri <aobarbieri@gmail.com>
2024-03-05 18:53:43 -0500

    Major updates on about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import avatar from '/assets/avatar.svg'
-import bg from '/assets/avatar-bg.png'
+import github from '/assets/brands/github.svg'
+import linkedin from '/assets/brands/linkedin.svg'
+import email from '/assets/icons/email.svg'
 
 export default function About() {
 	const [about, setAbout] = useState(null)
@@ -16,17 +18,40 @@ export default function About() {
 
 	const loaded = () => (
 		<section className='about'>
+			<div className='avatar'>
+				<img src={avatar} alt="Amanda's Avatar" />
+			</div>
 			<div className='about-content'>
-				<h1 className=''>Hi, I&apos;m {about.name}</h1>
-				<p>{about.bio}</p>
+				<h1>{about.name}</h1>
+				<p className='pitch'>{about.pitch}</p>
+				<div className='bio'>
+					<span>About</span>
+					<p>{about.bio}</p>
+					<button>Show more</button>
+				</div>
 			</div>
-
-			<div className='wrapper'>
-				<img className='avatar' src={avatar} alt="Amanda's Avatar" />
-				<img className='avatar-bg' src={bg} />
-			</div>
+			<div className='divider'></div>
+			<ul className='about-icons'>
+				<li className='list-item'>
+					<a href='https://www.linkedin.com/in/amanda-mitzian/' target='_blank' rel='noreferrer'>
+						<img src={linkedin} alt='' />
+						<span>LinkedIn</span>
+					</a>
+				</li>
+				<li className='list-item'>
+					<a href='https://github.com/aobarbieri' target='_blank' rel='noreferrer'>
+						<img src={github} alt='' />
+						<span>GitHub</span>
+					</a>
+				</li>
+				<li className='list-item'>
+					<a href='' target='_blank' rel='noreferrer'>
+						<img src={email} alt='' />
+						<span>Email</span>
+					</a>
+				</li>
+			</ul>
 		</section>
 	)
-
 	return about ? loaded() : ''
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,22 @@
-import { Link } from 'react-router-dom'
-import logo from '/assets/custom-tag.svg'
+import About from './About'
 
 export default function Header() {
 	return (
-		<header className='fixed'>
+		<header>
 			<nav className='navbar'>
-				<Link to='/'>
-					<img src={logo} alt='Colored HTML tag' />
-					<h3 className=''>
-						amanda<span>mitzian</span>
-					</h3>
-				</Link>
-
-				<ul className='flex nav-items'>
-					<li>
-						<Link to='/projects'>Projects</Link>
-					</li>
+				<ul className='nav-items'>
 					<li>
 						<a href='https://drive.google.com/file/d/1zhjTywdUQ9HmZyuEaP6MT1hUastRY-n9/view?usp=sharing' target='_blank' rel='noreferrer'>
 							Resume
 						</a>
 					</li>
-					{/* <li>Contact</li> */}
+					<li>
+						<span>/</span>
+					</li>
+					<li>Contact</li>
 				</ul>
 			</nav>
+			<About />
 		</header>
 	)
 }
