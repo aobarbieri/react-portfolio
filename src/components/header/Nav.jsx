452c13fae65909dd311e54f51beaa0a7ac625bb2
aobarbieri <aobarbieri@gmail.com>
2024-08-15 11:28:42 -0400

    update nav

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,17 +1,13 @@
 export default function Nav() {
-	return (
-		<nav className='navbar'>
-			<ul className='nav-items'>
-				<li>
-					<a href='https://drive.google.com/file/d/18H_1qMNBN2m6iHeO3amOJ9nuGU_somfC/view?usp=sharing' target='_blank' rel='noreferrer'>
-						Resume
-					</a>
-				</li>
-				<li>
-					<span>/</span>
-				</li>
-				<li>Contact</li>
-			</ul>
-		</nav>
-	)
+  return (
+    <nav className="navbar">
+      <ul className="nav-items">
+        <li>Resume</li>
+        <li>
+          <span>/</span>
+        </li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
 }
